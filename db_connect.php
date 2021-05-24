b98d9eb316f1ccb415d39189af72d5e11849aa04
nashterevati <55985064+nashterevati@users.noreply.github.com>
2021-05-24 11:33:20 +0530

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,4 +1,4 @@
 <?php
 $conn= new mysqli('localhost','root','','quiz_db')or die("Could not connect to mysql".mysqli_error($con));
-
+//$conn= new mysqli('remotemysql.com','LZkVfvb8nz','evduoxkmu8','LZkVfvb8nz')or die("Could not connect to mysql".mysqli_error($con));
 ?>
